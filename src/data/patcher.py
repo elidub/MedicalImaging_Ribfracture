ca32b86e43775979475a9efe1b0cb19acae7631d
AverageDude0815 <robin.sasse@gmx.net>
2023-10-01 10:38:26 +0200

    preprocess

--- src/data/patcher.py
+++ src/data/patcher.py
@@ -1,8 +1,4 @@
-import os
-from dataset import read_image
 import numpy as np
-import argparse
-from tqdm import tqdm
 
 
 def patch_volume(volume, patch_size, pad_value=0):
@@ -26,42 +22,3 @@ def patch_volume(volume, patch_size, pad_value=0):
 
     return patches
 
-
-def parse_option(notebook=False):
-
-    parser = argparse.ArgumentParser(description="RibFracPatcher")
-
-    parser.add_argument('--split', type=str, default='val', help='train, val, or test')
-    parser.add_argument('--data_dir', type=str, default='../data_dev', help='Path to data directory')
-    parser.add_argument('--save_dir', type=str, default='../data_dev_patches', help='Path to save directory')
-    parser.add_argument('--patch_size', type=int, nargs=3, default=[128, 128, 128], help='Patch size')
-
-    args = parser.parse_args() if not notebook else parser.parse_args(args=[])
-    return args
-
-
-def main(args):
-
-    if not os.path.exists(os.path.join(args.save_dir, args.split)):
-        os.makedirs(os.path.join(args.save_dir, args.split))
-        os.makedirs(os.path.join(args.save_dir, args.split, 'images'))
-        os.makedirs(os.path.join(args.save_dir, args.split, 'labels'))
-
-    print('Splitting {} image data into patches...'.format(args.split))
-    split_dir_images = os.path.join(args.data_dir, args.split, 'images')
-    for file in tqdm(os.listdir(split_dir_images)):
-        image_patches = patch_volume(read_image(os.path.join(split_dir_images, file))[0], args.patch_size)
-        np.save(os.path.join(args.save_dir, args.split, 'images', file.split('.')[0]), image_patches)
-
-    if args.split != 'test':
-        split_dir_labels = os.path.join(args.data_dir, args.split, 'labels')
-        print('Splitting {} label data into patches...'.format(args.split))
-        for file in tqdm(os.listdir(split_dir_labels)):
-            label_patches = patch_volume(read_image(os.path.join(split_dir_labels, file))[0], args.patch_size)
-            np.save(os.path.join(args.save_dir, args.split, 'labels', file.split('.')[0]), label_patches)
-
-
-if __name__ == '__main__':
-    args = parse_option()
-    print(args)
-    main(args)
---
+++ src/preprocess.py
@@ -0,0 +1,68 @@
+import os
+import argparse
+import numpy as np
+from tqdm import tqdm
+from data.dataset import read_image
+from data.patcher import patch_volume
+from data.seg2box import extract_boxes_from_patches
+from data.utils import normalize, simplify_labels
+
+
+def parse_option(notebook=False):
+
+    parser = argparse.ArgumentParser(description="RibFracPatcher")
+
+    parser.add_argument('--split', type=str, default='val', help='train, val, or test')
+    parser.add_argument('--data_dir', type=str, default='../data_dev/raw', help='Path to data directory')
+    parser.add_argument('--patch_size', type=int, nargs=3, default=[128, 128, 128], help='Patch size')
+
+    args = parser.parse_args() if not notebook else parser.parse_args(args=[])
+    return args
+
+
+def main(args):
+
+    patch_dir = os.path.join(args.data_dir, 'patches')
+    if not os.path.exists(os.path.join(patch_dir, args.split)):
+        os.makedirs(os.path.join(patch_dir, args.split))
+        os.makedirs(os.path.join(patch_dir, args.split, 'images'))
+        if args.split != 'test':
+            os.makedirs(os.path.join(patch_dir, args.split, 'labels'))
+
+    box_dir = os.path.join(args.data_dir, 'boxes')
+    if not os.path.exists(os.path.join(box_dir, args.split)):
+        os.makedirs(os.path.join(box_dir, args.split))
+        os.makedirs(os.path.join(box_dir, args.split, 'images'))
+        if args.split != 'test':
+            os.makedirs(os.path.join(box_dir, args.split, 'labels'))
+
+    print('Splitting {} data into patches and creating bounding boxes...'.format(args.split))
+    split_dir_images = os.path.join(args.data_dir, args.split, 'images')
+    for file in tqdm(os.listdir(split_dir_images)):
+        img_id = file.split('-')[0]
+        img_path = f"{args.data_dir}/{args.split}/images/{img_id}-image.nii.gz"
+        label_path = f"{args.data_dir}/{args.split}/labels/{img_id}-label.nii.gz"
+        img_data, _ = read_image(img_path)
+        img_data = normalize(img_data)
+        img_patches = patch_volume(img_data, args.patch_size)
+        np.save(os.path.join(patch_dir, args.split, 'images', f'{img_id}-image.npy'), img_patches)
+        if split_dir_images != 'test':
+            label_data, _ = read_image(label_path)
+            label_data = simplify_labels(label_data)
+            label_patches = patch_volume(label_data, args.patch_size)
+            np.save(os.path.join(patch_dir, args.split, 'labels', f'{img_id}-label.npy'), label_patches)
+            img_boxes, label_boxes = extract_boxes_from_patches(img_patches, label_patches)
+            np.save(os.path.join(box_dir, args.split, 'images', f'{img_id}-image.npy'), img_boxes)
+
+            print('img_data.shape', img_data.shape)
+            print('img_patches.shape', img_patches.shape)
+            print('img_boxes.shape', img_boxes.shape)
+            print('label_data.shape', label_data.shape)
+            print('label_patches.shape', label_patches.shape)
+            print('label_boxes.shape', label_boxes.shape)
+
+
+if __name__ == '__main__':
+    args = parse_option()
+    print(args)
+    main(args)
