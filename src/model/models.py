10e0dd810e44fb16b85af2a6170c810d3a65c53f
Elias Dubbeldam <elias@wearebit.com>
2023-10-26 15:20:05 +0200

    adding fracnet

--- src/model/models.py
+++ src/model/models.py
@@ -345,3 +345,83 @@ class RetinaNet3D(nn.Module):
         box_y = torch.cat([self.regressgion_block(f) for f in features], dim=1)
         cls_y = torch.cat([self.classification_block(f) for f in features], dim=1)
         return box_y, cls_y
+
+class FracNet(nn.Module):
+    def __init__(self, in_channels, num_classes, first_out_channels=16):
+        super().__init__()
+        self.first = ConvBlock(in_channels, first_out_channels)
+        in_channels = first_out_channels
+        self.down1 = Down(in_channels, 2 * in_channels)
+        self.down2 = Down(2 * in_channels, 4 * in_channels)
+        self.down3 = Down(4 * in_channels, 8 * in_channels)
+        self.up1   = Up(8 * in_channels, 4 * in_channels)
+        self.up2   = Up(4 * in_channels, 2 * in_channels)
+        self.up3   = Up(2 * in_channels, in_channels)
+        self.final = nn.Conv3d(in_channels, num_classes, 1)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv3d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='leaky_relu')
+            elif isinstance(m, nn.BatchNorm3d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = x.unsqueeze(1)
+        x1 = self.first(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x  = self.up1(x4, x3)
+        x  = self.up2(x, x2)
+        x  = self.up3(x, x1)
+        x  = self.final(x)
+        x = x.squeeze(1)
+        x = torch.sigmoid(x)
+        return x
+
+
+class ConvBlock(nn.Sequential):
+    def __init__(self, in_channels, out_channels):
+        super().__init__(
+            nn.Conv3d(in_channels, out_channels, 3, padding=1, bias=False),
+            nn.BatchNorm3d(out_channels),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv3d(out_channels, out_channels, 3, padding=1, bias=False),
+            nn.BatchNorm3d(out_channels),
+            nn.LeakyReLU(inplace=True)
+        )
+
+
+class Down(nn.Sequential):
+    def __init__(self, in_channels, out_channels):
+        super().__init__(
+            nn.MaxPool3d(2),
+            ConvBlock(in_channels, out_channels)
+        )
+
+
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv1 = ConvBlock(in_channels, out_channels)
+        self.conv2 = nn.Sequential(
+            nn.ConvTranspose3d(in_channels, out_channels, 2, stride=2, bias=False),
+            # nn.Upsample(scale_factor=2, mode="nearest"), # does not work (yet)
+            nn.BatchNorm3d(out_channels),
+            nn.LeakyReLU(inplace=True),
+        )
+
+    def forward(self, x, y):
+        x = self.conv2(x)
+        x = self.conv1(torch.cat([y, x], dim=1))
+        return x
+    
+class PrintLayer(nn.Module):
+    def __init__(self):
+        super(PrintLayer, self).__init__()
+    
+    def forward(self, x):
+        # Do your print / debug stuff here
+        print(x.shape)
+        return x
